227dac90fc68c921ec2ed909160c28bc4e548b5a
조병우 <whquddn55@gmail.com>
2022-01-14 18:46:11 +0900

    Init project

---
+++ android/app/src/main/kotlin/com/thuthi/PnuPlatoAdvancedBrowser/pnu_plato_advanced_browser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thuthi.PnuPlatoAdvancedBrowser.pnu_plato_advanced_browser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pnu_plato_advanced_browser/screens/landingScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PnuPlatoAdvancedBrowser',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LandingScreen(),
+      },
+    );
+  }
+}
