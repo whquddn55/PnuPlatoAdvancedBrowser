2dec029f93149ec905056b8a0bdea69146a00d77
조병우 <whquddn55@gmail.com>
2022-01-24 02:08:59 +0900

    Fix: OnlineAbsencePage의 data가 없는 경우 에러고침

--- lib/pages/platoPage/courseMainPage/onlineAbsencePage/onlineAbsencePage.dart
+++ lib/pages/platoPage/courseMainPage/onlineAbsencePage/onlineAbsencePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
 import 'package:pnu_plato_advanced_browser/controllers/courseController.dart';
 import 'package:pnu_plato_advanced_browser/pages/loadingPage.dart';
@@ -10,20 +9,16 @@ class OnlineAbsencePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final CourseController controller = Get.find<CourseController>();
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Viewer'),
+          title: const Text('온라인 출석부'),
           centerTitle: true,
         ),
         body: FutureBuilder(
-          future: controller.getVodStatus(courseId),
+          future: Get.find<CourseController>().getVodStatus(courseId),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               var data = snapshot.data! as Map<String, bool>;
-              if (data.keys.isEmpty) {
-                return const Center(child: Text('정보를 가져오는데 문제가 발상했습니다.', style: TextStyle(fontSize: 30, color: Colors.red)));
-              }
               return SingleChildScrollView(
                 child: Column(
                   children: data.entries.map((entry) {
