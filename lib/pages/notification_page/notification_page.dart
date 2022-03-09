8369f44a43ba59961e6279d79c1fd784cd0717a2
조병우 <whquddn55@gmail.com>
2022-03-10 00:58:08 +0900

    Fix: NotiificationPage에 MainDrawer가 안 뜨는 문제 해결 Fix: NotiificationPage가 로그인을 하지 않아도 뜨는 문제 해결

--- lib/pages/notification_page/notification_page.dart
+++ lib/pages/notification_page/notification_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pnu_plato_advanced_browser/controllers/login_controller.dart';
 import 'package:pnu_plato_advanced_browser/controllers/notification_controller.dart';
 import 'package:pnu_plato_advanced_browser/data/notification.dart' as noti;
 import 'package:pnu_plato_advanced_browser/main_appbar.dart';
+import 'package:pnu_plato_advanced_browser/main_drawer.dart';
 import 'package:pnu_plato_advanced_browser/pages/loading_page.dart';
 import 'package:pnu_plato_advanced_browser/pages/notification_page/sections/notification_tile.dart';
 
@@ -26,21 +29,44 @@ class _NotificationPageState extends State<NotificationPage> {
     if (notificationList.isEmpty) return const LoadingPage(msg: "로딩중 입니다...");
     return Scaffold(
       appBar: MainAppbar("알림"),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          var res = await NotificationController.fetchNotificationList();
-          setState(() {
-            notificationList = res;
-          });
+      drawer: const MainDrawer(),
+      body: GetBuilder<LoginController>(
+        builder: (controller) {
+          if (controller.loginStatus == false) {
+            return Builder(builder: (context) {
+              return GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                child: const Center(
+                    child: Text(
+                  '로그인이 필요합니다.',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                )),
+                onTap: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            });
+          } else {
+            return RefreshIndicator(
+              onRefresh: () async {
+                var res = await NotificationController.fetchNotificationList();
+                setState(() {
+                  notificationList = res;
+                });
+              },
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                shrinkWrap: true,
+                itemCount: notificationList.length,
+                itemBuilder: (context, index) {
+                  return NotificationTile(notification: notificationList[index], index: index);
+                },
+              ),
+            );
+          }
         },
-        child: ListView.builder(
-          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-          shrinkWrap: true,
-          itemCount: notificationList.length,
-          itemBuilder: (context, index) {
-            return NotificationTile(notification: notificationList[index], index: index);
-          },
-        ),
       ),
     );
   }
