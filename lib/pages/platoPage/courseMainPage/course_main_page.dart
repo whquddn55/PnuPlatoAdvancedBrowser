288601f22a3dbd0cc9a9f7f8b96c680b2d521a53
조병우 <whquddn55@gmail.com>
2022-03-11 13:24:33 +0900

    Update: ActivityButton 색깔로 Todo Status 표시하도록 업데이트 Update: CourseMainPage.dart 리팩토링 Update: CourseMainPage에서 각 Activity를 Divider로 구분

--- lib/pages/platoPage/courseMainPage/course_main_page.dart
+++ lib/pages/platoPage/courseMainPage/course_main_page.dart
@@ -30,6 +30,118 @@ class CourseMainPage extends StatelessWidget {
     TodoController.to.refreshTodoList([course.id], vodStatusList);
   }
 
+  Widget _renderEndDrawer(final BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(course.professor!.name),
+            accountEmail: null,
+            currentAccountPicture: CircleAvatar(backgroundImage: CachedNetworkImageProvider(course.professor!.iconUri.toString())),
+            otherAccountsPictures: [
+              IconButton(
+                icon: Icon(Icons.email, color: Get.theme.primaryIconTheme.color),
+                onPressed: () {
+                  /* TODO: 메시지 페이지로 이동 */
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('팀티칭/조교'),
+            expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+            children: course.assistantList.map((assistant) {
+              return TextButton.icon(
+                icon: CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(assistant.iconUri.toString()),
+                ),
+                label: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      assistant.name,
+                      style: TextStyle(color: Get.textTheme.bodyText1!.color),
+                    ),
+                    Text(
+                      assistant.type,
+                      style: TextStyle(color: Get.textTheme.bodyText1!.color),
+                    ),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  alignment: Alignment.centerLeft,
+                ),
+                onPressed: () {},
+              );
+            }).toList(),
+          ),
+          const Divider(height: 0, thickness: 1),
+          ListTile(
+            title: const Text('국문 계획표'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PlannerPage(
+                    title: '국문 계획표',
+                    uri: course.koreanPlanUri,
+                  ),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('영문 계획표'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PlannerPage(
+                    title: '영문 계획표',
+                    uri: course.englishPlanUri,
+                  ),
+                ),
+              );
+            },
+          ),
+          const Divider(height: 0, thickness: 1),
+          ListTile(
+            title: const Text('온라인 출석부'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => OnlineAbsencePage(courseId: course.id),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('스마트 출석부'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SmartAbsencePage(courseId: course.id),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('성적부'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GradePage(courseId: course.id),
+                  ));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final _articleTileController = ExpandedTileController();
@@ -54,115 +166,7 @@ class CourseMainPage extends StatelessWidget {
             centerTitle: true,
             leading: const BackButton(),
           ),
-          endDrawer: Drawer(
-            child: ListView(
-              children: [
-                UserAccountsDrawerHeader(
-                  accountName: Text(course.professor!.name),
-                  accountEmail: null,
-                  currentAccountPicture: CircleAvatar(backgroundImage: CachedNetworkImageProvider(course.professor!.iconUri.toString())),
-                  otherAccountsPictures: [
-                    IconButton(
-                      icon: Icon(Icons.email, color: Get.theme.primaryIconTheme.color),
-                      onPressed: () {
-                        /* TODO: 메시지 페이지로 이동 */
-                      },
-                    ),
-                  ],
-                ),
-                ExpansionTile(
-                  title: const Text('팀티칭/조교'),
-                  expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: course.assistantList.map((assistant) {
-                    return TextButton.icon(
-                      icon: CircleAvatar(
-                        backgroundImage: CachedNetworkImageProvider(assistant.iconUri.toString()),
-                      ),
-                      label: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            assistant.name,
-                            style: TextStyle(color: Get.textTheme.bodyText1!.color),
-                          ),
-                          Text(
-                            assistant.type,
-                            style: TextStyle(color: Get.textTheme.bodyText1!.color),
-                          ),
-                        ],
-                      ),
-                      style: TextButton.styleFrom(
-                        alignment: Alignment.centerLeft,
-                      ),
-                      onPressed: () {},
-                    );
-                  }).toList(),
-                ),
-                const Divider(height: 0, thickness: 1),
-                ListTile(
-                  title: const Text('국문 계획표'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PlannerPage(
-                          title: '국문 계획표',
-                          uri: course.koreanPlanUri,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: const Text('영문 계획표'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PlannerPage(
-                          title: '영문 계획표',
-                          uri: course.englishPlanUri,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                const Divider(height: 0, thickness: 1),
-                ListTile(
-                  title: const Text('온라인 출석부'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => OnlineAbsencePage(courseId: course.id),
-                      ),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: const Text('스마트 출석부'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SmartAbsencePage(courseId: course.id),
-                      ),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: const Text('성적부'),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GradePage(courseId: course.id),
-                        ));
-                  },
-                ),
-              ],
-            ),
-          ),
+          endDrawer: _renderEndDrawer(context),
           body: ListView(
             children: [
               ExpandedTile(
@@ -231,26 +235,25 @@ class CourseMainPage extends StatelessWidget {
                       ),
                       contentSeperator: 0,
                       title: Text(key),
-                      content: Material(
-                        child: Column(
-                          children: [
-                            course.summaryMap[key] == ''
-                                ? const SizedBox.shrink()
-                                : Container(
-                                    child: renderHtml(course.summaryMap[key]!),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(width: 1, color: Get.theme.hintColor),
-                                      borderRadius: BorderRadius.circular(3.0),
-                                    ),
-                                    margin: const EdgeInsets.only(bottom: 20.0),
+                      content: Column(
+                        children: [
+                          course.summaryMap[key] == ''
+                              ? const SizedBox.shrink()
+                              : Container(
+                                  child: renderHtml(course.summaryMap[key]!),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(width: 1, color: Get.theme.hintColor),
+                                    borderRadius: BorderRadius.circular(3.0),
                                   ),
-                            Column(
-                              children: course.activityMap[key]!
-                                  .map((activity) => ActivityButton(activity: activity, courseTitle: course.title, courseId: course.id))
-                                  .toList(),
-                            ),
-                          ],
-                        ),
+                                  margin: const EdgeInsets.only(bottom: 20.0),
+                                ),
+                          Column(
+                            children: course.activityMap[key]!.fold<List<Widget>>([], (prv, activity) {
+                              prv.addAll([const Divider(), ActivityButton(activity: activity, courseTitle: course.title, courseId: course.id)]);
+                              return prv;
+                            }),
+                          ),
+                        ],
                       ),
                     ),
                   ],
--- lib/pages/platoPage/courseMainPage/sections/activity_button.dart
+++ lib/pages/platoPage/courseMainPage/sections/activity_button.dart
@@ -1,11 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:pnu_plato_advanced_browser/common.dart';
+import 'package:pnu_plato_advanced_browser/controllers/todo_controller.dart';
 import 'package:pnu_plato_advanced_browser/data/course_activity.dart';
 import 'package:pnu_plato_advanced_browser/data/course_file.dart';
 import 'package:pnu_plato_advanced_browser/data/download_information.dart';
+import 'package:pnu_plato_advanced_browser/data/todo.dart';
 import 'package:pnu_plato_advanced_browser/pages/platoPage/courseMainPage/assignPage/assign_page.dart';
 import 'package:pnu_plato_advanced_browser/pages/platoPage/courseMainPage/boradPage/board_page.dart';
 import 'package:pnu_plato_advanced_browser/pages/platoPage/courseMainPage/sections/file_bottom_sheet.dart';
@@ -20,73 +23,83 @@ class ActivityButton extends StatelessWidget {
   final CourseActivity activity;
   const ActivityButton({Key? key, required this.activity, required this.courseTitle, required this.courseId}) : super(key: key);
 
+  Future<void> _tabEvent(final BuildContext context) async {
+    if (activity.type == 'ubboard') {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => BoardPage(boardId: activity.id, courseTitle: courseTitle, courseId: courseId)));
+    } else if (activity.type == 'vod') {
+      await showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          useRootNavigator: true,
+          builder: (context) => VodBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
+    } else if (activity.type == 'ubfile') {
+      await showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          useRootNavigator: true,
+          builder: (context) => FileBottomSheet(
+              file: CourseFile(imgUrl: activity.iconUri?.toString() ?? '', url: CommonUrl.fileViewerUrl + activity.id, title: activity.title),
+              fileType: DownloadType.activity,
+              courseTitle: courseTitle,
+              courseId: courseId));
+    } else if (activity.type == 'assign') {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (_) => AssignPage(
+                    assignId: activity.id,
+                    courseId: courseId,
+                    courseTitle: courseTitle,
+                  )));
+    } else if (activity.type == 'url') {
+      showModalBottomSheet(
+          context: context, isScrollControlled: true, useRootNavigator: true, builder: (context) => LinkBottomSheet(activity: activity));
+    } else if (activity.type == 'folder') {
+      showModalBottomSheet(
+          context: context,
+          useRootNavigator: true,
+          isScrollControlled: true,
+          builder: (context) => FolderBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
+    } else if (activity.type == 'zoom') {
+      showModalBottomSheet(
+          context: context,
+          useRootNavigator: true,
+          isScrollControlled: true,
+          builder: (context) => ZoomBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
+    } else {
+      await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: const Text("아직 구현이 안 됐어요! 괜찮으시다면 버그리포트로 어떤 내용을 클릭했는지 알려주세요!"),
+          actions: [TextButton(child: const Text("확인"), onPressed: () => Navigator.pop(context))],
+        ),
+      );
+      ChromeSafariBrowser().open(
+          url: Uri.parse(CommonUrl.courseUrlViewUrl + activity.id + '&redirect=1'),
+          options: ChromeSafariBrowserClassOptions(
+              android: AndroidChromeCustomTabsOptions(showTitle: false, toolbarBackgroundColor: Colors.white), ios: IOSSafariOptions()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     bool avilablity = activity.iconUri != null && activity.availablility == true;
-    return InkWell(
-      onTap: avilablity == false
-          ? null
-          : () async {
-              if (activity.type == 'ubboard') {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => BoardPage(boardId: activity.id, courseTitle: courseTitle, courseId: courseId)));
-              } else if (activity.type == 'vod') {
-                await showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    useRootNavigator: true,
-                    builder: (context) => VodBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
-              } else if (activity.type == 'ubfile') {
-                await showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    useRootNavigator: true,
-                    builder: (context) => FileBottomSheet(
-                        file:
-                            CourseFile(imgUrl: activity.iconUri?.toString() ?? '', url: CommonUrl.fileViewerUrl + activity.id, title: activity.title),
-                        fileType: DownloadType.activity,
-                        courseTitle: courseTitle,
-                        courseId: courseId));
-              } else if (activity.type == 'assign') {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => AssignPage(
-                              assignId: activity.id,
-                              courseId: courseId,
-                              courseTitle: courseTitle,
-                            )));
-              } else if (activity.type == 'url') {
-                showModalBottomSheet(
-                    context: context, isScrollControlled: true, useRootNavigator: true, builder: (context) => LinkBottomSheet(activity: activity));
-              } else if (activity.type == 'folder') {
-                showModalBottomSheet(
-                    context: context,
-                    useRootNavigator: true,
-                    isScrollControlled: true,
-                    builder: (context) => FolderBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
-              } else if (activity.type == 'zoom') {
-                showModalBottomSheet(
-                    context: context,
-                    useRootNavigator: true,
-                    isScrollControlled: true,
-                    builder: (context) => ZoomBottomSheet(activity: activity, courseTitle: courseTitle, courseId: courseId));
-              }
-            },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: activity.iconUri == null
-                      ? const SizedBox.shrink()
-                      : Opacity(
-                          opacity: avilablity == false ? 0.5 : 1.0,
-                          child: CachedNetworkImage(
+    return Opacity(
+      opacity: avilablity == false ? 0.5 : 1.0,
+      child: InkWell(
+        onTap: avilablity == false ? null : () async => await _tabEvent(context),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: activity.iconUri == null
+                        ? const SizedBox.shrink()
+                        : CachedNetworkImage(
                             imageUrl: activity.iconUri.toString(),
                             placeholder: (context, url) => const SizedBox(
                               width: 20,
@@ -97,52 +110,69 @@ class ActivityButton extends StatelessWidget {
                             height: 20,
                             width: 20,
                           ),
-                        ),
-                ),
-                Flexible(
-                  child: Text.rich(
-                    TextSpan(
-                      text: activity.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Get.theme.textTheme.bodyText2!.color!.withOpacity(avilablity == false ? 0.5 : 1.0),
-                      ),
-                      children: [
+                  ),
+                  Flexible(
+                    child: GetBuilder<TodoController>(builder: (controller) {
+                      Todo? sameTodo = controller.todoList.firstWhereOrNull((todo) => todo.courseId == activity.courseId && todo.id == activity.id);
+
+                      Color titleColor = Colors.black;
+                      if (sameTodo != null) {
+                        switch (sameTodo.status) {
+                          case TodoStatus.done:
+                            titleColor = Colors.green;
+                            break;
+                          case TodoStatus.undone:
+                          case TodoStatus.doing:
+                            titleColor = Colors.red;
+                            break;
+                        }
+                      }
+
+                      return Text.rich(
                         TextSpan(
-                          text: '   ${activity.info}',
-                          style: const TextStyle(
-                            color: Colors.blueAccent,
-                            fontWeight: FontWeight.normal,
+                          text: activity.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: titleColor,
                           ),
-                        ),
-                        if (activity.startDate != null)
-                          if (activity.lateDate != null)
+                          children: [
                             TextSpan(
-                              text:
-                                  '\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.startDate!)} ~ ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.endDate!)}\n(지각: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.lateDate!)})',
+                              text: '   ${activity.info}',
                               style: const TextStyle(
-                                color: Colors.orangeAccent,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            )
-                          else
-                            TextSpan(
-                              text:
-                                  '\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.startDate!)} ~ ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.endDate!)}',
-                              style: const TextStyle(
-                                color: Colors.orangeAccent,
+                                color: Colors.blueAccent,
                                 fontWeight: FontWeight.normal,
                               ),
                             ),
-                      ],
-                    ),
+                            if (activity.startDate != null)
+                              if (activity.lateDate != null)
+                                TextSpan(
+                                  text:
+                                      '\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.startDate!)} ~ ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.endDate!)}\n(지각: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.lateDate!)})',
+                                  style: const TextStyle(
+                                    color: Colors.orangeAccent,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                )
+                              else
+                                TextSpan(
+                                  text:
+                                      '\n${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.startDate!)} ~ ${DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.endDate!)}',
+                                  style: const TextStyle(
+                                    color: Colors.orangeAccent,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                          ],
+                        ),
+                      );
+                    }),
                   ),
-                ),
-              ],
-            ),
-            if (activity.availablilityInfo != '') renderHtml(activity.availablilityInfo),
-            if (activity.description != '') renderHtml(activity.description)
-          ],
+                ],
+              ),
+              if (activity.availablilityInfo != '') renderHtml(activity.availablilityInfo),
+              if (activity.description != '') renderHtml(activity.description)
+            ],
+          ),
         ),
       ),
     );
