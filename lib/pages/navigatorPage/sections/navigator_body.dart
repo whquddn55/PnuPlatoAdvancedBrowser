f2818a72b4da400c817b9fdb4ad45d9917b1a5fa
thuthi <whquddn55@gmail.com>
2022-03-21 19:30:44 +0900

    Update: 다른 Tab에서 뒤로가기 시 PlatoPage로 이동하도록 수정 Update: 뒤로가기 2번 시 종료되도록 수정

--- lib/pages/navigatorPage/sections/navigator_body.dart
+++ lib/pages/navigatorPage/sections/navigator_body.dart
@@ -1,48 +1,73 @@
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'package:pnu_plato_advanced_browser/controllers/download_controller.dart';
 import 'package:pnu_plato_advanced_browser/controllers/route_controller.dart';
-import 'package:pnu_plato_advanced_browser/data/download_information.dart';
 
-class NavigatorBody extends StatelessWidget {
+class NavigatorBody extends StatefulWidget {
   const NavigatorBody({Key? key}) : super(key: key);
+
+  @override
+  State<NavigatorBody> createState() => _NavigatorBodyState();
+}
+
+class _NavigatorBodyState extends State<NavigatorBody> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    final TabController tabController = TabController(length: RouteController.to.pages.length, vsync: this);
+    DateTime? currentBackPressTime;
     return GetBuilder<RouteController>(
       builder: (controller) {
         return WillPopScope(
           onWillPop: () async {
-            return !(await controller.maybePop());
+            bool res = !(await controller.maybePop());
+
+            if (res == true) {
+              if (controller.currentIndex != 0) {
+                controller.currentIndex = 0;
+                tabController.animateTo(0);
+                res = false;
+              }
+            }
+            if (res == true) {
+              DateTime now = DateTime.now();
+              if (currentBackPressTime == null || now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
+                currentBackPressTime = now;
+                Fluttertoast.showToast(msg: "버튼을 한번 더 누르면 종료됩니다.", backgroundColor: Colors.black.withOpacity(0.5));
+                res = false;
+              }
+            }
+            return res;
           },
-          child: DefaultTabController(
-            length: controller.pages.length,
-            child: Scaffold(
-              body: TabBarView(
-                children: controller.pages,
-                physics: const NeverScrollableScrollPhysics(),
-              ),
-              bottomNavigationBar: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 200),
-                reverseDuration: const Duration(milliseconds: 50),
-                child: TabBar(
-                  onTap: (index) => controller.currentIndex = index,
-                  indicator: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(
-                        color: Get.theme.colorScheme.secondary,
-                        width: 3.0,
-                      ),
+          child: Scaffold(
+            body: TabBarView(
+              controller: tabController,
+              children: controller.pages,
+              physics: const NeverScrollableScrollPhysics(),
+            ),
+            bottomNavigationBar: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              reverseDuration: const Duration(milliseconds: 50),
+              child: TabBar(
+                controller: tabController,
+                onTap: (index) {
+                  controller.currentIndex = index;
+                  //tabController.animateTo(index);
+                },
+                indicator: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: Get.theme.colorScheme.secondary,
+                      width: 3.0,
                     ),
                   ),
-                  tabs: const [
-                    Tab(child: Icon(Icons.home)),
-                    Tab(child: Icon(Icons.calendar_today_outlined)),
-                    Tab(child: Icon(Icons.email)),
-                    Tab(child: Icon(Icons.download_rounded)),
-                    Tab(child: Icon(Icons.notifications)),
-                  ],
                 ),
+                tabs: const [
+                  Tab(child: Icon(Icons.home)),
+                  Tab(child: Icon(Icons.calendar_today_outlined)),
+                  Tab(child: Icon(Icons.email)),
+                  Tab(child: Icon(Icons.download_rounded)),
+                  Tab(child: Icon(Icons.notifications)),
+                ],
               ),
             ),
           ),
